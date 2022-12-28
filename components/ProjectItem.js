46b4b92ea33e10a45c56efb18fe0e077e024ff2a
Naing Linn Khant <nainglinnkhnt@gmail.com>
2022-12-28 13:13:01 +0630

    Modify project item

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -11,36 +11,32 @@ export default function ProjectItem({ project, link = '#' }) {
     const span = `col-span-${project.span}`
 
     return (
-        <div className={`${span} ${styles['project-item']}`}>
-            <a href={link} className={styles['project-item-link']}>
-                <div className={styles.overlay}></div>
+        <a href={link} className={`${span} ${styles['project-item-link']}`}>
+            <div className={styles.overlay}></div>
 
-                <div className={styles['project-item-image-container']}>
-                    <img
-                        src={`/${project.image}.jpg`}
-                        alt='Project Image'
-                        className={styles['project-item-image']}
-                    />
+            <img
+                src={`/${project.image}.jpg`}
+                alt='Project Image'
+                className={styles['project-item-image']}
+            />
+
+            <div className={styles['project-item-info']}>
+                <div className={project.contrastText ? styles['contrast-text'] : ''}>
+                    <p>{project.category}</p>
+                    <h2>{project.name}</h2>
                 </div>
 
-                <div className={styles['project-item-info']}>
-                    <div className={project.contrastText ? styles['contrast-text'] : ''}>
-                        <p>{project.category}</p>
-                        <h2>{project.name}</h2>
-                    </div>
-
-                    <ul className={styles['project-item-platforms']}>
-                        {icons.map((icon, index) => (
-                            <li
-                                className='flex justify-center items-center rounded-full bg-white w-8 h-8 relative mr-2.5'
-                                key={index}
-                            >
-                                <Image src={icon} width='18px' height='18px' alt='Icon' />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </a>
-        </div>
+                <ul className={styles['project-item-platforms']}>
+                    {icons.map((icon, index) => (
+                        <li
+                            className='flex justify-center items-center rounded-full bg-white w-8 h-8 relative mr-2.5'
+                            key={index}
+                        >
+                            <Image src={icon} width='18px' height='18px' alt='Icon' />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </a>
     )
 }
