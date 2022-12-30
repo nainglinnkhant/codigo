2b9b01fff1416cc89f037aac607c8dee87044c30
Naing Linn Khant <nainglinnkhnt@gmail.com>
2022-12-30 14:09:42 +0630

    Upgrade next version and add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
+import { Poppins } from '@next/font/google'
 import { Navbar, PageTransition } from '../components'
 import '../styles/globals.scss'
 
+const poppins = Poppins({
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    subsets: ['latin'],
+})
+
 function MyApp({ Component, pageProps }) {
     return (
-        <PageTransition>
-            <Navbar />
-            <Component {...pageProps} />
-        </PageTransition>
+        <main className={poppins.className}>
+            <PageTransition>
+                <Navbar />
+                <Component {...pageProps} />
+            </PageTransition>
+        </main>
     )
 }
 
