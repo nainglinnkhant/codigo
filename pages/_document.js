e82e57ab7310288e4e9f83d241b5d9ec6b1a44d6
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-01-01 19:32:07 +0630

    Remove @next/font and integrate the font with link

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,16 @@ class MyDocument extends Document {
                         crossOrigin='anonymous'
                         referrerPolicy='no-referrer'
                     />
+                    <link rel='preconnect' href='https://fonts.googleapis.com' />
+                    <link
+                        rel='preconnect'
+                        href='https://fonts.gstatic.com'
+                        crossOrigin='anonymous'
+                    />
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+                        rel='stylesheet'
+                    />
                 </Head>
 
                 <body>
